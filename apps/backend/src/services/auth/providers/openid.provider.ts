642e3b6c648b0b6b5997f6e6a1127647430b1a9a
Filipe Ramalho <privat@filiperamalho.de>
2024-10-11 11:34:56 +0200

    Fix oAuth flow

--- apps/backend/src/services/auth/providers/openid.provider.ts
+++ apps/backend/src/services/auth/providers/openid.provider.ts
@@ -3,13 +3,13 @@ import { ProvidersInterface } from '@gitroom/backend/services/auth/providers.int
 export class OpenIDProvider implements ProvidersInterface {
   private clientId: string = process.env.OAUTH_CLIENT_ID!;
   private clientSecret: string = process.env.OAUTH_CLIENT_SECRET!;
-  private redirectUri: string = encodeURIComponent('${process.env.FRONTEND_URL}/settings');
+  private redirectUri: string = encodeURIComponent(`${process.env.FRONTEND_URL}/settings`);
   private authUrl: string = process.env.OAUTH_AUTH_URL!;
   private tokenUrl: string = process.env.OAUTH_TOKEN_URL!;
   private userInfoUrl: string = process.env.OPENID_USER_INFO_URL!;
 
   generateLink(): string {
-    return `${this.authUrl}?client_id=${this.clientId}&response_type=code&scope=openid%20email&redirect_uri=${this.redirectUri}`;
+    return `${this.authUrl}?client_id=${this.clientId}&response_type=code&scope=openid%20email&redirect_uri=${this.redirectUri}&state=`;
   }
 
   async getToken(code: string): Promise<string> {
@@ -22,7 +22,7 @@ export class OpenIDProvider implements ProvidersInterface {
         body: new URLSearchParams({
           client_id: this.clientId,
           client_secret: this.clientSecret,
-          code,
+          code: code,
           grant_type: 'authorization_code',
           redirect_uri: this.redirectUri,
         }).toString(),
